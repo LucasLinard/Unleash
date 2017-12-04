211087e0c8de0d5067077bfdd4791364ea0cfe37
Lucas Linard <contato@linard.tech>
2017-12-03 22:25:12 -0200

    Google Login Working

--- app/src/main/java/tech/linard/android/unleash/activities/LoginActivity.java
+++ app/src/main/java/tech/linard/android/unleash/activities/LoginActivity.java
@@ -3,20 +3,19 @@ package tech.linard.android.unleash.activities;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -29,11 +28,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,6 +66,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int RC_SIGN_IN = 9001;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -65,6 +76,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private Button mEmailSignInButton;
     private Button mEmailLogInButton;
     private Button mEmailLogOutButton;
+
+    private SignInButton mGoogleSignInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     public void onStart() {
@@ -101,12 +116,29 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailLogInButton = (Button) findViewById(R.id.email_log_in);
         mEmailLogOutButton = (Button) findViewById(R.id.email_log_out);
+        mGoogleSignInButton = findViewById(R.id.google_sign_in_button);
+
         mEmailSignInButton.setOnClickListener(this);
         mEmailLogInButton.setOnClickListener(this);
         mEmailLogOutButton.setOnClickListener(this);
+        mGoogleSignInButton.setOnClickListener(this);
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+
+        // Configure Google Sign In
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+
+
     }
 
     private void populateAutoComplete() {
@@ -120,10 +152,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mEmailLogOutButton.setVisibility(View.GONE);
             mEmailLogInButton.setVisibility(View.VISIBLE);
             mEmailSignInButton.setVisibility(View.VISIBLE);
+            mGoogleSignInButton.setVisibility(View.VISIBLE);
         } else {
             mEmailLogOutButton.setVisibility(View.VISIBLE);
             mEmailLogInButton.setVisibility(View.GONE);
             mEmailSignInButton.setVisibility(View.GONE);
+            mGoogleSignInButton.setVisibility(View.GONE);
         }
     }
 
@@ -282,6 +316,56 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 });
     }
 
+    private void signInWithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
     private boolean isEmailValid(String email) {
         return email.contains("@");
     }
@@ -382,6 +466,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 FirebaseAuth.getInstance().signOut();
                 updateUI(null);
                 break;
+            case R.id.google_sign_in_button:
+                signInWithGoogle();
         }
 
     }
